f0b5d6834eb689c8b52344535dcae1514aa5f118
Tejas Shah <shah.te@husky.neu.edu>
2020-02-05 19:53:18 -0500

    Spring Initializer project

---
+++ bdi-demo/src/main/java/com/tejas/bdidemo/BdiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tejas.bdidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdiDemoApplication.class, args);
+	}
+
+}
---
+++ bdi-demo/src/test/java/com/tejas/bdidemo/BdiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejas.bdidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
