d8e3ac695a4fe52690fc9bf4daaed18bc05c1fd1
Tejas Shah <shah.te@husky.neu.edu>
2020-02-07 21:52:41 -0500

    Updated save method with exception handling and json schema validation

--- bdi-demo/src/main/java/com/tejas/bdidemo/controller/PlanController.java
+++ bdi-demo/src/main/java/com/tejas/bdidemo/controller/PlanController.java
@@ -1,29 +1,45 @@
 package com.tejas.bdidemo.controller;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
+import com.tejas.bdidemo.exception.BadRequestException;
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.ValidationException;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
+
 
 @RestController
 @RequestMapping("/plan")
 public class PlanController {
 
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity savePlan(@RequestBody String json) {
-        Gson gson = new Gson();
-        JsonObject plan = gson.fromJson(json, JsonObject.class);
+    @ResponseStatus(value = HttpStatus.CREATED)
+    @RequestMapping(method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String savePlan(@RequestBody String json) {
+
+        JSONObject plan = new JSONObject(json);
+
+        // Validate the plan against the schema
+        JSONObject plan_schema = new JSONObject(new JSONTokener(PlanController.class.getResourceAsStream("/plan-schema.json")));
+
+        Schema schema = SchemaLoader.load(plan_schema);
+        try {
+            schema.validate(plan);
+        } catch (ValidationException e) {
+            throw new BadRequestException(e.getMessage());
+        }
+
+
         JedisPool jedisPool = new JedisPool();
         Jedis jedis = jedisPool.getResource();
-        String objectId = plan.get("objectId").getAsString();
+        String objectId = (String) plan.get("objectId");
         jedis.set("Plan:" + objectId, plan.toString());
         jedis.close();
-        return new ResponseEntity(HttpStatus.CREATED);
+        return "";
     }
 }
